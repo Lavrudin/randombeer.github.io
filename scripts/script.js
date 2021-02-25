148eb72b86fe548ea5d268ed03868338ba975bda
Durval <durvalhenriquesouza93@gmail.com>
2021-02-25 17:55:28 +0000

    Links para compra foram adcionados

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ console.log(randomBtn);
 randomBtn.addEventListener("click", (randomBeer) => {
   let cardContainer = document.getElementById("beforeClick");
   let cardPhrase = document.getElementById("cardPhrase");
+  let orderNow = document.getElementById("orderNow");
 
   let beerCatalog = [
     "Stella",
@@ -36,4 +37,42 @@ randomBtn.addEventListener("click", (randomBeer) => {
     cardContainer.style.fontSize = "33px";
   }
   cardContainer.innerHTML = `<h1>${randomCatalogItem}</h1>`;
+
+  switch (randomCatalogItem) {
+    case "Stella":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/stella-artois' target='_blank'>Clique para pedir a sua ${randomCatalogItem}</a>`;
+      break;
+    case "Beck's":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/becks' target='_blank'>Clique para pedir a sua ${randomCatalogItem}.</a>`;
+      break;
+    case "Corona":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/corona' target='_blank'>Clique para pedir a sua ${randomCatalogItem}</a>`;
+      break;
+    case "Bohemia":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/bohemia' target='_blank'>Clique para pedir a sua ${randomCatalogItem}</a>`;
+      break;
+    case "Colorado":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/colorado' target='_blank'>Clique para pedir a sua ${randomCatalogItem}</a>`;
+      break;
+    case "Brahma":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/brahma' target='_blank'>Clique para pedir a sua ${randomCatalogItem}</a>`;
+      break;
+    case "Skol":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/skol' target='_blank'>Clique para pedir a sua ${randomCatalogItem}</a>`;
+      break;
+    case "Budweiser":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/budweiser' target='_blank'>Clique para pedir a sua ${randomCatalogItem}</a>`;
+      break;
+    case "Antarctica":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/antarctica' target='_blank'>Clique para pedir a sua ${randomCatalogItem}</a>`;
+      break;
+    case "Original":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/original' target='_blank'>Clique para pedir a sua ${randomCatalogItem}</a>`;
+      break;
+    case "Duplo Malte":
+      orderNow.innerHTML = `<a href='https://www.ze.delivery/produtos/marca/brahma' target='_blank'>Clique para pedir a sua ${randomCatalogItem}</a>`;
+      break;
+    default:
+      console.log("HOUVE UM ERRO");
+  }
 });
